afc0d5233fb32cd27911b7298010e894986620ba
erik landerholm <elanderholm@gmail.com>
2020-04-03 12:38:17 -0700

    breadfast demo

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Lebron")
-option_b = os.getenv('OPTION_B', "Jordan")
+option_a = os.getenv('OPTION_A', "Breadfast")
+option_b = os.getenv('OPTION_B', "Amazon Fresh")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
